9cc0015c08a80190815f3340ea9b68bf9191d7d1
carefree <946715806@qq.com>
2020-06-13 17:11:40 +0800

    first commit

---
+++ GradientProgressViewExample/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  GradientProgressViewExample
+//
+//  Created by carefree on 2020/6/13.
+//  Copyright © 2020 carefree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var progressView1: GradientProgressView!
+    @IBOutlet weak var progressView2: GradientProgressView!
+    @IBOutlet weak var progressView3: GradientProgressView!
+    @IBOutlet weak var progressView4: GradientProgressView!
+    
+    @IBOutlet weak var tagView: UIImageView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //设置圆角
+        progressView1.layer.cornerRadius = 5
+        progressView2.layer.cornerRadius = 0
+        progressView3.layer.cornerRadius = 10
+        progressView4.layer.cornerRadius = 4
+        
+        //设置进度条圆角
+        progressView1.progressCornerRadius = 5
+        progressView2.progressCornerRadius = 0
+        progressView3.progressCornerRadius = 6
+        progressView4.progressCornerRadius = 3
+        
+        //设置内间距
+        progressView3.progressEdgeInsets = UIEdgeInsets(top: 4, left: 5, bottom: 4, right: 5)
+        progressView4.progressEdgeInsets = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
+        
+        //设置纯色和渐变色
+        progressView1.progressColors = [.green]
+        progressView2.progressColors = [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)]
+        progressView3.progressColors = [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)]
+        progressView4.progressColors = [.red]
+        
+        //动画时间
+        progressView1.animationDuration = 1
+        progressView2.animationDuration = 0.2
+        progressView3.animationDuration = 1.2
+        progressView4.animationDuration = 1.8
+        
+        //动画函数
+        progressView1.timingFunction = CAMediaTimingFunction(name: .default)
+        progressView2.timingFunction = CAMediaTimingFunction(name: .linear)
+        progressView3.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        progressView4.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        //更新回调
+        progressView4.progressUpdating = {[unowned self] progress, frame in
+            //更新Label的百分比
+            let text = String(format: "%.0f", progress * 100)
+            self.progressLabel.text = "\(text)%"
+            
+            //计算出标签的移动位置
+            let newFrame = self.progressView4.convert(frame, to: self.view)
+            let size: CGFloat = 36
+            let tagFrame = CGRect(x: (newFrame.origin.x + newFrame.size.width) - size / 2.3, y: newFrame.origin.y - size, width: size, height: size)
+            self.tagView.frame = tagFrame
+            self.progressLabel.sizeToFit()
+            self.progressLabel.center = CGPoint(x: self.tagView.center.x, y: self.tagView.center.y - 7)
+        }
+        
+        //设置标签的初始位置
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+            self.resetPosition()
+        }
+    }
+    
+    @IBAction func resetAction(_ sender: Any) {
+        progressView1.progress = 0
+        progressView2.progress = 0
+        progressView3.progress = 0
+        progressView4.progress = 0
+        
+        resetPosition()
+    }
+    
+    @IBAction func startAction(_ sender: Any) {
+        progressView1.setProgress(0.2 + progressView1.progress, animated: true)
+        progressView2.setProgress(0.25 + progressView2.progress, animated: true)
+        progressView3.setProgress(0.3 + progressView3.progress, animated: true)
+        progressView4.setProgress(0.35 + progressView4.progress, animated: true)
+    }
+    
+    func resetPosition() {
+        let frame = self.progressView4.frame
+        let size: CGFloat = 36
+        let tagFrame = CGRect(x: frame.origin.x - size / 2.3, y: frame.origin.y - size, width: size, height: size)
+        self.tagView.frame = tagFrame
+        self.progressLabel.sizeToFit()
+        self.progressLabel.center = CGPoint(x: self.tagView.center.x, y: self.tagView.center.y - 7)
+        self.progressLabel.text = "0%"
+    }
+    
+}
+
---
+++ Sources/GradientProgressView.swift
@@ -0,0 +1,146 @@
+//
+//  GradientProgressView.swift
+//  GradientProgressViewExample
+//
+//  Created by carefree on 2020/6/13.
+//  Copyright © 2020 carefree. All rights reserved.
+//
+
+import UIKit
+
+open class GradientProgressView: UIView {
+    
+    //进度条完成部分的渐变颜色，设置单个为纯色，设置多个为渐变色
+    public var progressColors: [UIColor] = [.systemBlue] {
+        didSet {
+            if progressColors.count == 0 {
+                gradientLayer.colors = nil
+            } else if progressColors.count == 1 {
+                let color = progressColors[0]
+                gradientLayer.colors = [color, color].map { $0.cgColor }
+            } else {
+                gradientLayer.colors = progressColors.map { $0.cgColor }
+            }
+        }
+    }
+    
+    //进度条完成部分的圆角半径
+    public var progressCornerRadius: CGFloat = 0 {
+        didSet {
+            maskLayer.cornerRadius = progressCornerRadius
+        }
+    }
+    
+    //进度完成部分的内间距
+    public var progressEdgeInsets: UIEdgeInsets = .zero {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    //当前进度
+    public var progress: Float {
+        get {
+            return privateProgress
+        }
+        set {
+            setProgress(newValue, animated: false)
+        }
+    }
+    
+    //渐变Layer
+    public let gradientLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.anchorPoint = .zero
+        layer.startPoint = .zero
+        layer.endPoint = CGPoint(x: 1.0, y: 0.0)
+
+        return layer
+    }()
+    
+    //动画持续时间
+    public var animationDuration: TimeInterval = 0.3
+    
+    //动画时间函数
+    public var timingFunction: CAMediaTimingFunction = CAMediaTimingFunction(name: .default)
+    
+    //进度更新动画过程中的回调，在这里可以拿到当前进度及进度条的frame
+    public var progressUpdating: ((Float, CGRect) -> ())?
+    
+    
+    private var privateProgress: Float = 0
+    private var displayLink: CADisplayLink?
+    private let maskLayer: CALayer = {
+        let layer = CALayer()
+        layer.backgroundColor = UIColor.white.cgColor
+
+        return layer
+    }()
+    
+    // MARK: - Lifecycle
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        commonInit()
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        gradientLayer.frame = bounds.inset(by: progressEdgeInsets)
+        var bounds = gradientLayer.bounds
+        bounds.size.width *= CGFloat(progress)
+        maskLayer.frame = bounds
+    }
+    
+    // MARK: - Private
+    private func commonInit() {
+        let color = progressColors[0]
+        gradientLayer.colors = [color, color].map { $0.cgColor }
+        gradientLayer.mask = maskLayer
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    @objc private func displayLinkAction() {
+        guard let frame = maskLayer.presentation()?.frame else { return }
+        let progress = frame.size.width / gradientLayer.frame.size.width
+        progressUpdating?(Float(progress), frame)
+    }
+    
+    // MARK: - Public
+    public func setProgress(_ progress: Float, animated: Bool) {
+        let validProgress = min(1.0, max(0.0, progress))
+        if privateProgress == validProgress {
+            return
+        }
+        privateProgress = validProgress
+        
+        if privateProgress > 0 {
+            //开启CADisplayLink
+            displayLink = CADisplayLink(target: self, selector: #selector(displayLinkAction))
+            displayLink?.add(to: .main, forMode: .default)
+        }
+        
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(animated ? animationDuration : 0)
+        CATransaction.setAnimationTimingFunction(timingFunction)
+        CATransaction.setCompletionBlock {
+            self.displayLink?.invalidate()
+            self.displayLink = nil
+        }
+        defer {
+            CATransaction.commit()
+        }
+        
+        //更新maskLayer的frame
+        var bounds = gradientLayer.bounds
+        bounds.size.width *= CGFloat(privateProgress)
+        maskLayer.frame = bounds
+    }
+}
